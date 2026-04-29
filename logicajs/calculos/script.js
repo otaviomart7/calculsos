c5e9a70cbebc78aa8b775889b30a3ad042a6b667
otaviomart7 <otaviomart7tds@gmail.com>
2026-04-29 08:42:21 -0300

    Add files via upload

---
+++ logicajs/calculos/script.js
@@ -0,0 +1,8 @@
+function somar(){
+    var numero1 = document.getElementById('n1').valueAsNumber
+    var numero2 = document.getElementById('n2').valueAsNumber
+    var numero3 = document.getElementById('n3').valueAsNumber
+    var numero4 = document.getElementById('n4').valueAsNumber
+    var res = document.getElementById('res')
+    res.textContent = numero1 + numero2 + numero3 + numero4
+}
